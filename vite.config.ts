8bc3ddaeb4f732d7468d0769bd92b67d1b5a21b1
Sajal Saxena <119779950+saxenasajal03@users.noreply.github.com>
2026-09-25 00:07:38 +0530

    Update base path for build command in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: process.env.VITE_BASE_PATH || (command === 'build' ? '/' : '/'),
+  base: command === 'build' ? '/bnk-digital/' : '/',
   plugins: [react()],
   server: {
     port: 5174,
